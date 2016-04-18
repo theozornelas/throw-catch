0357dcc0dd48b20b73b0250e106d4f10795a1859
Ethan Slattery <Ethan.Slattery@yahoo.com>
2016-04-17 22:23:31 -0700

    develop - cleaned up test folder a bit and added things to ignore file

--- test/test_dbmanager.cpp
+++ test/test_dbmanager.cpp
@@ -1,22 +1,30 @@
-#include <QtTest/QtTest>
+#ifndef Test_DBManager_H
+#define Test_DBManager_H
+
+#include "TestRunner.h"
+#include <QObject>
 
 class Test_DBManager : public QObject
 {
-    Q_OBJECT
+ Q_OBJECT
 private slots:
-    // Put testing slots here
-    void initTest();
+    /*** GENERAL TEST SETUP AND TEARDOWN ***/
+    // Called before every test, if it fails then the NEXT test does not run
+    void initTestCase() { qDebug("initTestCase() is called before each test!"); }
+    // Called after the last test function was executed.
+    void cleanupTestCase() {}
 
-private:
-    // Put testing variables here
+    /*** PER-TEST SETUP AND TEARDOWN ***/
+    // Called before the first test function is executed. If this fails no tests are run
+    void init() { qDebug("init() is called before every function!"); }
+    // Called after every test function
+    void cleanup() { qDebug("cleanup() runs after every test!"); }
+
+    /*** TESTS FOR THE CLASS ***/
+    //void initTest();
+    void testingTest() { qDebug("*** RUNNING THE ACTUAL TEST ***"); QVERIFY(true); }
+
 };
 
-void Test_DBManager::initTest(){
-    QVERIFY(true);
-}
-
-//#endif //TEST_DATABASE_H
-
-QTEST_MAIN(Test_DBManager)
-#include "test_dbmanager.moc"
-
+DECLARE_TEST(Test_DBManager)
+#endif // Test_DBManager_H
