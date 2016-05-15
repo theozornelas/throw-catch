a12f5b1ae7ea3f17d6dc1c7a2f90f03267daa853
Connor Peace <connorjpeace@gmail.com>
2016-05-15 14:26:59 -0700

    Getting an 'index out of range' error when trying to use the comparator sort

--- src/header/quicksort.h
+++ src/header/quicksort.h
@@ -1,11 +1,20 @@
 #ifndef QUICKSORT_H
 #define QUICKSORT_H
 
-// Prototype
+/**************
+ * PROTOTYPES *
+ **************/
 template <typename E>
 void QuickSort(QVector<E> &vecToSort, int left, int right);
 
-// Definition
+template <typename E, typename Comparator>
+void QuickSort(QVector<E> &vecToSort, int left, int right, Comparator compare);
+
+/***************
+ * DEFINITIONS *
+ ***************/
+
+//Without comparator
 template <typename E>
 void QuickSort(QVector<E> &vecToSort, int left, int right)
 {
@@ -41,6 +50,44 @@ void QuickSort(QVector<E> &vecToSort, int left, int right)
         QuickSort(vecToSort, left, j);
     if(i < right)
         QuickSort(vecToSort, i, right);
+}
+
+//With comparator
+template <typename E, typename Comparator>
+void QuickSort(QVector<E> &vecToSort, int left, int right, Comparator compare)
+{
+    int i = left;
+    int j = right;
+    E temp;
+
+    //Chooses the pivot
+    E pivot = vecToSort[(left + right) / 2];
+
+    //Sorts the vector
+    while(i <= j)
+    {
+        while(compare(vecToSort[i], pivot))
+        {
+            ++i;
+        }
+        while(!compare(vecToSort[j], pivot))
+        {
+            --j;
+        }
+        if(i <= j)
+        {
+            temp = vecToSort[i];
+            vecToSort[i] = vecToSort[j];
+            vecToSort[j] = temp;
+            ++i;
+            --j;
+        }
+    }
+
+    if(left < j)
+        QuickSort(vecToSort, left, j, compare);
+    if(i < right)
+        QuickSort(vecToSort, i, right, compare);
 }
 
 
--- test/sortingtests.h
+++ test/sortingtests.h
@@ -27,8 +27,11 @@ private slots:
     /*** TESTS FOR THE CLASS ***/
 //    void test_SortEmptyVector();
     void test_SortIntegers();
+    void test_SortIntegersWithComparator();
     void test_SortDates();
+    void test_SortDatesWithComparator();
     void test_SortStadiums();
+    void test_SortStadiumsWithComparator();
 
 
 
@@ -79,7 +82,7 @@ void SortingTests::cleanup() {
  *********************************************************************/
 void SortingTests::test_SortIntegers() {
 
-    //Initializes the QVector
+    // Initializes the QVector
     srand(time(NULL));
     QVector<int> testVector;
     int temp;
@@ -88,12 +91,38 @@ void SortingTests::test_SortIntegers() {
         testVector.push_back(temp);
     }
 
-    //Sorts the vector
+    // Sorts the vector
     QuickSort(testVector, 0, testVector.size()-1);
 
-    //Checks to see if it sorted properly
+    // Checks to see if it sorted properly
     for(int i = 0; i < testVector.size()-1; ++i) {
         QVERIFY(testVector[i] <= testVector[i+1]);
+    }
+}
+
+/*********************************************************************
+ * Tests sorting a QVector of integers using the QuickSort function
+ * with a comparator passed in
+ *
+ * FAILS IF: - The QVector is not sorted from greatest to least
+ *********************************************************************/
+void SortingTests::test_SortIntegersWithComparator() {
+
+    // Initializes the QVector
+    srand(time(NULL));
+    QVector<int> testVector;
+    int temp;
+    for(int i = 0; i < 100; ++i) {
+        temp = rand() % 100;
+        testVector.push_back(temp);
+    }
+
+    // Sorts the vector
+    QuickSort(testVector, 0, testVector.size()-1, std::greater<int>());
+
+    // Checks to see if it sorted properly
+    for(int i = 0; i < testVector.size()-1; ++i) {
+        QVERIFY(testVector[i] >= testVector[i+1]);
     }
 }
 
@@ -104,7 +133,7 @@ void SortingTests::test_SortIntegers() {
  *********************************************************************/
 void SortingTests::test_SortDates() {
 
-    //Initializes the QVector
+    // Initializes the QVector
     QVector<QDate> testVector;
 
     testVector.push_back(QDate(2015, 10, 5));
@@ -115,12 +144,40 @@ void SortingTests::test_SortDates() {
     testVector.push_back(QDate(2007, 6, 20));
     testVector.push_back(QDate(2007, 1, 1));
 
-    //Sorts the vector
+    // Sorts the vector
     QuickSort(testVector, 0, testVector.size()-1);
 
-    //Checks to see if it sorted properly
+    // Checks to see if it sorted properly
     for(int i = 0; i < testVector.size()-1; ++i) {
         QVERIFY(testVector[i] <= testVector[i+1]);
+    }
+}
+
+/*********************************************************************
+ * Tests sorting a QVector of QDates using the QuickSort function
+ * with a comparator passed in
+ *
+ * FAILS IF: - The QVector is not sorted from greatest to least
+ *********************************************************************/
+void SortingTests::test_SortDatesWithComparator() {
+
+    // Initializes the QVector
+    QVector<QDate> testVector;
+
+    testVector.push_back(QDate(2015, 10, 5));
+    testVector.push_back(QDate(2008, 10, 5));
+    testVector.push_back(QDate(2009, 10, 5));
+    testVector.push_back(QDate(2007, 1, 15));
+    testVector.push_back(QDate(2007, 2, 5));
+    testVector.push_back(QDate(2007, 6, 20));
+    testVector.push_back(QDate(2007, 1, 1));
+
+    // Sorts the vector
+    QuickSort(testVector, 0, testVector.size()-1, std::greater<QDate>());
+
+    // Checks to see if it sorted properly
+    for(int i = 0; i < testVector.size()-1; ++i) {
+        QVERIFY(testVector[i] >= testVector[i+1]);
     }
 }
 
@@ -131,7 +188,37 @@ void SortingTests::test_SortDates() {
  *********************************************************************/
 void SortingTests::test_SortStadiums() {
 
-    //Initializes the QVector
+    // Initializes the QVector
+    QVector<Stadium> testVector;
+
+    testVector.push_back(Stadium(99));
+    testVector.push_back(Stadium(88));
+    testVector.push_back(Stadium(77));
+    testVector.push_back(Stadium(66));
+    testVector.push_back(Stadium(55));
+    testVector.push_back(Stadium(44));
+    testVector.push_back(Stadium(33));
+    testVector.push_back(Stadium(22));
+    testVector.push_back(Stadium(11));
+
+    // Sorts the vector
+    QuickSort(testVector, 0, testVector.size()-1);
+
+    // Checks to see if it sorted properly
+    for(int i = 0; i < testVector.size()-1; ++i) {
+        QVERIFY(testVector[i] <= testVector[i+1]);
+    }
+}
+
+/*********************************************************************
+ * Tests sorting a QVector of Stadiums using the QuickSort function
+ * with a comparator passed in
+ *
+ * FAILS IF: - The QVector is not sorted from greatest to least
+ *********************************************************************/
+void SortingTests::test_SortStadiumsWithComparator() {
+
+    // Initializes the QVector
     QVector<Stadium> testVector;
 
     testVector.push_back(Stadium(11));
@@ -144,14 +231,73 @@ void SortingTests::test_SortStadiums() {
     testVector.push_back(Stadium(88));
     testVector.push_back(Stadium(99));
 
-    //Sorts the vector
-    QuickSort(testVector, 0, testVector.size()-1);
+    // Sorts the vector
+    QuickSort(testVector, 0, testVector.size()-1, std::greater<Stadium>());
 
-    //Checks to see if it sorted properly
+    // Checks to see if it sorted properly
     for(int i = 0; i < testVector.size()-1; ++i) {
-        QVERIFY(testVector[i] <= testVector[i+1]);
+        QVERIFY(testVector[i] >= testVector[i+1]);
     }
 }
+
+///*********************************************************************
+// * Tests sorting a QVector on an int, QDate, Stadium, and Edge of various types using the QuickSort
+// * function with a comparator.
+// *
+// * FAILS IF: - The QVectors are not sorted from greatest to least
+// *********************************************************************/
+//void SortingTests::test_SortWithComparator() {
+
+//    // Initializes the int vector
+//    srand(time(NULL));
+//    QVector<int> intVector;
+//    int temp;
+//    for(int i = 0; i < 100; ++i) {
+//        temp = rand() % 100;
+//        intVector.push_back(temp);
+//    }
+
+//    // Initializes the Date vector
+//    QVector<QDate> dateVector;
+//    dateVector.push_back(QDate(2015, 10, 5));
+//    dateVector.push_back(QDate(2008, 10, 5));
+//    dateVector.push_back(QDate(2009, 10, 5));
+//    dateVector.push_back(QDate(2007, 1, 15));
+//    dateVector.push_back(QDate(2007, 2, 5));
+//    dateVector.push_back(QDate(2007, 6, 20));
+//    dateVector.push_back(QDate(2007, 1, 1));
+
+//    // Initializes the Stadium Vector
+//    QVector<Stadium> stadiumVector;
+//    stadiumVector.push_back(Stadium(11));
+//    stadiumVector.push_back(Stadium(22));
+//    stadiumVector.push_back(Stadium(33));
+//    stadiumVector.push_back(Stadium(44));
+//    stadiumVector.push_back(Stadium(55));
+//    stadiumVector.push_back(Stadium(66));
+//    stadiumVector.push_back(Stadium(77));
+//    stadiumVector.push_back(Stadium(88));
+//    stadiumVector.push_back(Stadium(99));
+
+//    // Sorts the vectors
+//    QuickSort(intVector, 0, intVector.size()-1, std::greater<int>());
+//    QuickSort(dateVector, 0, dateVector.size()-1, std::greater<QDate>());
+//    QuickSort(stadiumVector, 0, stadiumVector.size()-1, std::greater<Stadium>());
+
+//    // Checks to see they were sorted properly
+//    for(int i = 0; i < intVector.size()-1; ++i) {
+//        QVERIFY(intVector[i] >= intVector[i+1]);
+//    }
+
+//    for(int i = 0; i < dateVector.size()-1; ++i) {
+//        QVERIFY(dateVector[i] >= dateVector[i+1]);
+//    }
+
+//    for(int i = 0; i < stadiumVector.size()-1; ++i) {
+//        QVERIFY(stadiumVector[i] >= stadiumVector[i+1]);
+//    }
+
+//}
 
 /*** THIS ADDS THE TEST TO THE LIST OF CLASSES TO RUN ***/
 DECLARE_TEST(SortingTests)
